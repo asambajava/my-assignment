6efe9719a255edc791d2fc19ca41d9ba84c68078
asambajava <96311074+asambajava@users.noreply.github.com>
2023-10-08 19:21:25 -0400

    selenium assignments
    selenium driver automation

--- src/main/java/com/assignment/my_assignment/App.java
+++ src/main/java/com/assignment/my_assignment/App.java
@@ -1,5 +1,10 @@
 package com.assignment.my_assignment;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 /**
  * Hello world!
  *
@@ -13,5 +18,36 @@ public class App
         System.out.println(cal.sum(30, 40));
         System.out.println(cal.divide(3, 3));
         System.out.println(cal.subtract(40, 40));
+        //initialize the drive
+        System.setProperty("webdriver.chrome.driver","/Users/asambaraju/Desktop/Devops_Demo/chromedriver-mac-arm64/chromedriver");
+        
+        //set chrome parameters
+        ChromeOptions chromeOptions = new ChromeOptions();
+        
+        //initialize web driver 
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        
+        //open the URL in the browser
+        driver.get("https://www.facebook.com/");
+        try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        //enter the email and password then click login
+        driver.findElement(By.id("email")).sendKeys("sambaraju_adithya@gmail.com");
+                
+        driver.findElement(By.id("password")).sendKeys("magic");
+
+        driver.findElement(By.id("u_0_5_p7")).click();
+        try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        driver.close();
     }
 }
